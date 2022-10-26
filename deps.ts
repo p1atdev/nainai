e0034a40b43f9f92db51fb67a5442e928599060a
unknown <git@p1at.dev>
2022-10-26 18:24:51 +0900

    chore: cleanup

--- deps.ts
+++
@@ -1,7 +0,0 @@
-// Deno's standard modules are here: https://deno.land/std
-
-// server
-export * from "https://deno.land/std@0.157.0/http/server.ts"
-
-// test
-export * from "https://deno.land/std@0.157.0/testing/asserts.ts"
